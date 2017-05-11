308dc33b5a0392dcbe01724dfc08af03166434dd
ChefDK User <chef@ip-10-91-31-175.ec2.internal>
2017-05-11 19:43:42 +0000

    Release version 0.2.0

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -2,5 +2,13 @@ package 'tree' do
  action :install
 end
 file "/etc/motd" do
- content "Property of Nuria\n"
+ content "Property of Nuria
+ IPADDRESS: #{node['ipaddress']}
+ HOSTNAME : #{node['hostname']}
+ MEMORY   : #{node['memory']['total']}
+ CPU      : #{node['cpu']['0']['mhz']}
+\n"
+ mode '0644'
+ owner 'root'
+ group 'root'
 end
